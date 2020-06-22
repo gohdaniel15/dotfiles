eb27e250334fcf6143d3fd698dca90c0abc65d8b
Daniel Goh <gohdaniel15@gmail.com>
2020-06-22 22:02:10 +0800

    Add CoC

--- setup.sh
+++ setup.sh
@@ -6,17 +6,17 @@ echo "[setup.sh] Locating/Installing Vim"
 if [ -f "$(command -v Vim)" ]
 then
   echo "[setup.sh] Vim already installed, upgrading to latest version"
-  brew upgrade vim
+  # brew upgrade vim
 else
   echo "[setup.sh] Cannot find path to Vim, installing Vim"
   brew install vim
 fi
 
 echo "[setup.sh] Copying vim configurations"
-cp ./vim/.vimrc ~/
+ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
 
 echo "[setup.sh] Installing vim plugins"
 vim +PluginInstall! +qall
 
 echo "[setup.sh] Installing ag silver_searcher for fuzzy finding"
-brew install the_silver_searcher
+# brew install the_silver_searcher
