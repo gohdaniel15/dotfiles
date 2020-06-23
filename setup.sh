fa7b512658f1944b5ed5782ddb20a356dd86dba0
Daniel Goh <gohdaniel15@gmail.com>
2020-06-23 21:03:50 +0800

    Touch up both setup files

--- setup.sh
+++ setup.sh
@@ -3,10 +3,10 @@ echo "[setup.sh] Running danielgoh's dev environment setup"
 
 # Install Vim
 echo "[setup.sh] Locating/Installing Vim"
-if [ -f "$(command -v Vim)" ]
+if [ -f "$(command -v vim)" ]
 then
   echo "[setup.sh] Vim already installed, upgrading to latest version"
-  # brew upgrade vim
+  brew upgrade vim
 else
   echo "[setup.sh] Cannot find path to Vim, installing Vim"
   brew install vim
@@ -15,8 +15,17 @@ fi
 echo "[setup.sh] Copying vim configurations"
 ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
 
+echo "[setup.sh] Creating necessary swap and backup folders for vim"
+mkdir ~/.vim/swap
+mkdir ~/.vim/backup
+mkdir ~/.vim/undo
+
 echo "[setup.sh] Installing vim plugins"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall! +qall
 
 echo "[setup.sh] Installing ag silver_searcher for fuzzy finding"
-# brew install the_silver_searcher
+if [ !-f "$(command -v Vim)" ]
+then
+  brew install the_silver_searcher
+fi
--- setup_linux.sh
+++ setup_linux.sh
@@ -3,7 +3,7 @@ echo "[setup.sh] Running danielgoh's dev environment setup"
 
 # Install Vim
 echo "[setup.sh] Locating/Installing Vim"
-if [ -f "$(command -v Vim)" ]
+if [ -f "$(command -v vim)" ]
 then
   echo "[setup.sh] Vim already installed, upgrading to latest version"
 else
@@ -15,9 +15,9 @@ echo "[setup.sh] Copying vim configurations"
 ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
 
 echo "[setup.sh] Creating necessary swap and backup folders for vim"
-mkdir ~/.vim/swap 
-mkdir ~/.vim/backup 
-mkdir ~/.vim/undo 
+mkdir ~/.vim/swap
+mkdir ~/.vim/backup
+mkdir ~/.vim/undo
 
 echo "[setup.sh] Installing vim plugins"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
